f89405cbf2e749b70f6572e4fada1fa49d295bb4
Dave Lane <dave@davelane.nz>
2018-09-02 20:35:41 +1200

    working PHP 7.2 configuration

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -4,33 +4,37 @@ set -e
 
 SEMAPH=composer-running
 
-if ! [ -f $SEMAPH ] ; then 
-    # create the semaphore file with the date in it... 
+# if it's not already there, install composer.
+if ! [ -e /usr/local/bin/composer ]; then
+    echo >&2 "first getting Composer"
+    # Get Composer
+    curl -S https://getcomposer.org/installer | php
+    chmod a+x composer.phar
+    mv composer.phar /usr/local/bin/composer
+fi
+if ! [ -e .git/hooks ]; then
+    echo >&2 "creating a .git/hooks dir to avoid errors"
+    mkdir -p .git/hooks
+fi
+
+if ! [ -f $SEMAPH ] ; then
+    # create the semaphore file with the date in it...
     date > $SEMAPH
+
 
     # run composer to set up dependencies if not already there...
     if ! [ -e vendor/autoload.php ]; then
         echo >&2 "installing dependencies with Composer"
-        if ! [ -e /usr/local/bin/composer ]; then
-            echo >&2 "first getting Composer"
-            # Get Composer
-            curl -S https://getcomposer.org/installer | php
-            chmod a+x composer.phar
-            mv composer.phar /usr/local/bin/composer
-        fi
-        if ! [ -e .git/hooks ]; then
-            echo >&2 "creating a .git/hooks dir to avoid errors"
-            mkdir -p .git/hooks
-        fi
         composer install
     else
-        echo >&2 "vendor dependencies already in place."
+        echo >&2 "vendor dependencies already in place, updating."
+        composer update
     fi
 
     #remove semaphore
     rm $SEMAPH
 else
-    echo >&2 "Looks like another composer is already running. If not, please remove $SEMAPH" 
-fi    
+    echo >&2 "Looks like another composer is already running. If not, please remove $SEMAPH"
+fi
 
 exec "$@"
